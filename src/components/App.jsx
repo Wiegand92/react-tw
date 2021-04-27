12826292506dea887e58250ad33f936d3b46d6dd
wiegand92 <wiegand.alex@gmail.com>
2021-04-27 14:20:31 -0400

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+      <div className="app">
+        <h1>Hello World!</h1>
+        <p>Check out the README <a href='https://github.com/Wiegand92/RegexSite'>here</a></p>
+      </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+// 3rd Party //
+
+import React from 'react';
+import reactDOM from 'react-dom';
+
+// Import tailwind first to give our styles preference //
+
+import 'tailwindcss/tailwind.css'
+
+// Styles //
+import './styles/style.scss';
+
+// Components //
+import App from './components/App';
+
+
+// Render //
+reactDOM.render(<App />, document.getElementById('app'));
