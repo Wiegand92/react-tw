e7e38d484c818bcdda6a59a2257c85ee6d1cfab1
wiegand92 <wiegand.alex@gmail.com>
2021-04-29 16:31:17 -0400

    update dependencies, change start script

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 // 3rd Party //
-
 import React from 'react';
 import reactDOM from 'react-dom';
 
-// Import tailwind first to give our styles preference //
-
-import 'tailwindcss/tailwind.css'
-
 // Styles //
+  // Import tailwind first to give our styles preference //
+import 'tailwindcss/tailwind.css';
 import './styles/style.scss';
 
 // Components //
 import App from './components/App';
-
 
 // Render //
 reactDOM.render(<App />, document.getElementById('app'));
